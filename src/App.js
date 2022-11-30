f33d9549259c5af49f1ddb9e4870b03169054a4b
pasong0511 <pasong0511@gmail.com>
2022-12-01 03:00:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState("");
+
+    return (
+        <div className="container">
+            <div className="mb-3">
+                <label className="form-label">Title</label>
+                <input
+                    className="form-control"
+                    value={title}
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                    }}
+                />
+            </div>
+            <button className="btn btn-primary">Post</button>
+        </div>
+    );
 }
 
 export default App;
